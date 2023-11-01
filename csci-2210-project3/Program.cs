20e7eb721e08978fa7ae27581f504505f63a2426
Brendan Dalhover <64229724+SparkSages@users.noreply.github.com>
2023-11-01 14:48:58 -0400

    Made the Main method asynchronous

--- csci-2210-project3/Program.cs
+++ csci-2210-project3/Program.cs
@@ -2,9 +2,10 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
+       
     }
 }
